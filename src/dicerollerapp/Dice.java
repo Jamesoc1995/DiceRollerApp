e78ad1ab6a6fddad967409db0e6a4481af9552ff
james <james@LAPTOP-CHRQ3ML1.Home>
2017-09-27 20:22:02 +0100

    new commit

---
+++ src/dicerollerapp/Dice.java
@@ -0,0 +1,52 @@
+package dicerollerapp;
+
+import java.util.Random;
+
+
+public class Dice {
+                   
+    Random rand = new Random();
+        int a; //attack roll
+        int d; //damage
+        int hp = 60; //enemy hp
+        int strBonus = 5; //strentgh bonus
+        int prof = 2; //proficeny
+        int ac = 35; //armor class 
+        
+        
+        
+    public void compute(){
+
+        for (int i = 0; i < 50; i++) {
+            a = rand.nextInt(20) + 1; //makes random number in the loop. making a new number every loop
+            if (a == 20) {
+                System.out.println("NAT 20! CRIT!");              
+            } else {
+                System.out.println("rolled a " + a + " Total roll is " + (a + strBonus + prof));
+            }
+            int roll = a + strBonus + prof;
+            if (roll >= ac) {
+                d = rand.nextInt(10) + 1;
+                if (a == 20) {
+                    d = d * 2; //double damage
+                }
+
+                //HIT
+                System.out.println("HIT for " + d + " damage");
+                hp = hp - d; //takeaway hp
+
+                if (hp <= 0) {
+                    System.out.println("Enemy is killed");
+                    System.exit(0);
+                }
+
+                System.out.println("Enemy has " + hp);
+            } 
+            //MISS
+            else {
+                System.out.println("MISS!");
+            }
+    }
+        
+    }
+}
---
+++ src/dicerollerapp/DiceRollerApp.java
@@ -0,0 +1,17 @@
+package dicerollerapp;
+
+/*Author James O'Carroll
+ *
+*This App is incomplete and it's purpose is for pratice and small revision of basic Java programming
+*This App is based on an role playing game where dice roles are crucial to playing.
+*This App will demonstrate forloops, random intergers, basic mathimatics.
+*/
+public class DiceRollerApp {
+
+    public static void main(String[] args) {
+        
+    Dice score = new Dice(); //new object called Score 
+    score.compute(); //execudes all the code below. i made the code in Dice class.
+    
+   }
+}
